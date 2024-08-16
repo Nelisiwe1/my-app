304467025eb9ef96a634b119ec75de85b54e60cc
Nelisiwe <nelisiwengqeme3@gmail.com>
2024-08-16 05:11:44 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import Header from './Components/Header';
-
+import Footer from './Components/Footer';
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <section id="home">
-          <h1>Home</h1>
-          <p>Welcome to the Home Page</p>
-        </section>
-        <section id="about">
-          <h1>About</h1>
-          <p>About Us Page</p>
-        </section>
-        <section id="services">
-          <h1>Services</h1>
-          <p>Our Services</p>
-        </section>
-        <section id="contact">
-          <h1>Contact</h1>
-          <p>Contact Us</p>
-        </section>
-      </main>
+    <Footer />      
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,13 +9,19 @@ const Navbar = () => {
       <h1 className="logo">MyLogo</h1>
       <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">CV Builder</a></li>
+        <li><a href="#services">Cover Letter</a></li>
+        <li><a href="#services">About</a></li>
+        <li><a href="#contact">Contact US</a></li>
       </ul>
       <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
         {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
       </button>
+      <div className="auth-buttons">
+        <button className="btn login-btn">Login</button>
+        <button className="btn signup-btn">Sign Up</button>
+      </div>
+      
     </nav>
   );
 };
